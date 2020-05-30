6376493b84f9b645a5719ae88ae0f86343d3d6af
Bharat Narayanasamy <bharatnarayanasamy@bharats-mbp.attlocal.net>
2020-05-30 00:16:05 -0700

    Added mouse movement

--- player.js
+++ player.js
@@ -67,6 +67,14 @@ class Player extends Phaser.GameObjects.Sprite {
     pointerMove(scene) {
         var angleToPointer = Phaser.Math.Angle.Between(this.x, this.y, scene.input.activePointer.worldX, scene.input.activePointer.worldY);
         var angleDelta = Phaser.Math.Angle.Wrap(angleToPointer - this.rotation);
+
+        var xdistance = scene.input.activePointer.worldX - this.x;
+        var ydistance = scene.input.activePointer.worldY - this.y;
+
+        this.body.setVelocityX(gameSettings.playerSpeed * xdistance/(Math.sqrt(xdistance*xdistance+ydistance*ydistance)))
+        this.body.setVelocityY(gameSettings.playerSpeed * ydistance/(Math.sqrt(xdistance*xdistance+ydistance*ydistance)))
+
+
         //some fancy math stuff I got from online
         if (Phaser.Math.Within(angleDelta, 0, gameSettings.TOLERANCE)) {
             this.rotation = angleToPointer;
