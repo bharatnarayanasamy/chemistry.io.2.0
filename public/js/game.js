9f70d419395b230ecf75ef45412c1ad4b60573e5
Krishna2182 <kdasari2002@gmail.com>
2020-12-28 20:23:19 -0600

    test: game 20fps, server 60 fps

--- public/js/game.js
+++ public/js/game.js
@@ -681,7 +681,7 @@ function create() {
     var t1 = 0;
     //displays other players' movement on screenloc
     this.socket.on('playerMoved', function (playerInfo) {
-        tickRate = 30;
+        tickRate = 20;
         // actual code
         var past = 1000 / tickRate;
 
